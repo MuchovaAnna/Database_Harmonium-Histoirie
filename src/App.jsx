ea36775277d25c157d4b0cace1d92b46a5b12121
Anna Muchova <muchova.anna@post.cz>
2024-10-24 18:38:35 +0200

    Creating a new Vite project and configuration MantineUI, SASS and Supabase

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <MantineProvider>
+        
+    </MantineProvider>
+    </>
+  )
+}
+
+export default App
